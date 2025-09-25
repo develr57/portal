251c026aee500176b4b87c64d56752cdf661dad1
develr57 <develr57@mail.ru>
2025-09-25 20:34:24 +0500

    правка имортов

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -11,6 +11,8 @@ from alembic import context
 
 from config import settings
 from models.base import Base
+from models.companies import Companies
+from models.departments import Departments
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
