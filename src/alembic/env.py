92c212c298c6ebc159aa99f881b954b1328cd869
develr57 <develr57@mail.ru>
2025-10-03 16:02:40 +0500

    Добавлен импорт модели Employees в env.py alembic

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -13,6 +13,7 @@ from config import settings
 from models.base import Base
 from models.companies import Companies
 from models.departments import Departments
+from models.employees import Employees
 from models.inst_points import InstPoints
 from models.instr_types import InstrTypes
 from models.instruments import Instruments
